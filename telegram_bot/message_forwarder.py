77fc801c5da878faf12f4705284e0662eac03720
kmm <kmmiil95@gmail.com>
2023-08-28 23:02:29 +0300

    forward message instead of send message on forwarder

--- telegram_bot/message_forwarder.py
+++ telegram_bot/message_forwarder.py
@@ -40,14 +40,14 @@ class MessageForwarder:
                 await bot.send_message(event.chat_id, 'Booked!')
                 to_chats = i.to_chats.split(',')
                 for i in to_chats:
-                    await bot.send_message(int(i.strip()), event.raw_text)
+                    await bot.forward_messages(int(i.strip()), event.id, int(event.chat_id))
 
         async def to_from_forwarding(event):
             forwardings = await sync_to_async(list)(Forwarding.objects.all().filter(to_chats__contains=event.chat_id))
             for i in forwardings:
                 logger.info(f"forwarded {i.from_chat} to {i.to_chats}")
                 await bot.send_message(event.chat_id, 'Sending something back!')
-                await bot.send_message(int(i.from_chat.strip()), event.raw_text)
+                await bot.forward_messages(int(i.from_chat.strip()), event.id, event.chat_id)
 
         await bot.run_until_disconnected()
 
