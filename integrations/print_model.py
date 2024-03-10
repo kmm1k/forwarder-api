502abc2829b0537b38b4a1cd80209a5f907f5779
Karl-Martin Miil <kmmiil95@gmail.com>
2024-03-10 21:24:15 +0100

    fix asian bug

--- integrations/print_model.py
+++ integrations/print_model.py
@@ -48,7 +48,7 @@ class PrintModel:
         if parsed_bet_class == "ah" or parsed_bet_class == "asian_handicap":
             bet_changed = True
 
-        if parsed_bet_type == "2" and "1x2" not in parsed_bet_class and "ml" not in parsed_bet_class:
+        if parsed_bet_type == "2" and "1x2" not in parsed_bet_class and "ml" not in parsed_bet_class and "asian" not in parsed_bet_class:
             self.mod = mod * -1
             self.display_team = away_team
             bet_changed = True
